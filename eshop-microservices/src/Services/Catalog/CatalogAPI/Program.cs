cc9ea79761f4e1cc503b0ad61db588925fbf6d72
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2024-07-19 15:16:22 +0700

    Add Routes (Carter), success POST /products

--- eshop-microservices/src/Services/Catalog/CatalogAPI/Program.cs
+++ eshop-microservices/src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // DI services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => {
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // hhtp req pipelines
+app.MapCarter();
 
 app.Run();
