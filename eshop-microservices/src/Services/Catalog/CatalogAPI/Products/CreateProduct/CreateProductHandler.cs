2d909fccbd71880725cebbc9fc8071d6abc9938c
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2024-07-19 13:44:17 +0700

    Change Mediatr Dependency to BuildingBlocks , CQRS , CreateProductHandler without DB connection

--- eshop-microservices/src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,33 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
 
 namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        :IRequest<CreateProductResult>;
+        :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
 
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // logic create product
-            throw new NotImplementedException();
+            // logic create product entity command object
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImaegeFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // save to db (skip dulu, langsung result)
+
+            // return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
